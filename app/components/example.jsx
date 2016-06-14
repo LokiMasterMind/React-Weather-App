2efbacd2df173ba61b8459b774fc6a41608d4aeb
Lokesh <chandawarlokesh@gmail.com>
2016-06-14 12:21:30 +0530

    example file updated

--- app/components/example.jsx
+++ app/components/example.jsx
@@ -18,7 +18,13 @@ var Example=(props) =>
 {    return(
       <div className="container">
         <div  className="jumbotron">
-          <h2>Example Component</h2>
+          <h2>Examples</h2>
+            Pune:<br/>
+            <p>It's it 29.21 in Pune</p><br/>
+            Napgur:<br/>
+            <p>It's it 37.51 in Nagpur</p><br/>
+            sdfhgasfjgskaghlushadg:<br/>
+            <p>Error: Error: Not found city!</p><br/>
         </div>
     </div>
     );
